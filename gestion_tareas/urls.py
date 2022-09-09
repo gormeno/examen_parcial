b4220160bdd8d9dd53a1a3118df1a73914fc950b
Gustavo Ormeño <gustavo.ormeno@pucp.edu.pe>
2022-09-09 03:29:44 -0500

    Quinto commit

--- gestion_tareas/urls.py
+++ gestion_tareas/urls.py
@@ -5,7 +5,10 @@ app_name = 'gestion_tareas'
 
 urlpatterns = [
     path('ingresar',views.ingresar,name='ingresar'),
-    path('dashboard',views.dashboard,name='dashboard'),
+    path('dashboard/<str:responsable>',views.dashboard,name='dashboard'),
     path('detalle_tarea/<str:ind>',views.detalle_tarea,name='detalle_tarea'),
-    path('editar_tarea/<str:ind>',views.editar_tarea,name='editar_tarea')
+    path('editar_tarea/<str:ind>',views.editar_tarea,name='editar_tarea'),
+    path('crear_tarea/<str:responsable>',views.crear_tarea,name='crear_tarea'),
+    path('eliminar_tarea/<str:ind>',views.eliminar_tarea,name='eliminar_tarea'),
+    path('finalizar_tarea/<str:ind>',views.finalizar_tarea,name='finalizar_tarea')
 ]
--- gestion_tareas/views.py
+++ gestion_tareas/views.py
@@ -1,8 +1,13 @@
+from asyncio import as_completed
+from datetime import datetime
+from xmlrpc.client import DateTime
 from django.shortcuts import render
 from http.client import HTTPResponse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from .models import tarea, usuarios_app
+from dateutil.parser import parse
+
 
 # Create your views here.
 
@@ -20,14 +25,36 @@ def ingresar(request):
                 usuario_registrado = 1
         #Fin validación
 
-        lista_tareas = tarea.objects.all()
+ #       lista_tareas = tarea.objects.filter(responsable=emailUsuario)
+ #       lista_tareas = lista_tareas.exclude(estado='ELIMINADO')
 
         if usuario_registrado == 1:
-#            return HttpResponseRedirect(reverse('gestion_tareas:dashboard', kwargs={'lista_tareas' : lista_tareas}))
+            #Actualización de estados de tareas
+            tareas_actualizar = tarea.objects.filter(responsable=emailUsuario) #extraer las tareas en progreso
+            for cadaTarea in tareas_actualizar:
+                #fecha_f = datetime.strptime(cadaTarea.fecha_entrega,'%Y-%m-%d').date()
+                fecha_f = cadaTarea.fecha_entrega
+                fecha_i = datetime.today().date()
+                delta = (fecha_f - fecha_i).days
+                #delta = abs(cadaTarea.fecha_entrega - datetime.date.today).days
+                if delta < 2:
+                    cadaTarea.estado = "FINALIZANDO"
+                    cadaTarea.save()
+            tareas_finalizando = tarea.objects.filter(responsable=emailUsuario,estado="FINALIZANDO")
+            #extraer todas las tareas del usuarios finalizando
+            for cadaTarea in tareas_finalizando:
+                fecha_ent = cadaTarea.fecha_entrega
+                fecha_hoy = datetime.today().date()
+                delta = (fecha_hoy - fecha_ent).days
+                if delta >= 1:
+                    cadaTarea.estado = "PENDIENTE"
+            #FIn de actualización de estados
+            return HttpResponseRedirect(reverse('gestion_tareas:dashboard', kwargs={'responsable' : emailUsuario}))
 #            return HttpResponseRedirect(reverse('gestion_tareas:dashboard'))
-            return render(request,'gestion_tareas/dashboard.html',{
-                'lista_tareas':lista_tareas
-            })
+#            return render(request,'gestion_tareas/dashboard.html',{
+#                'lista_tareas':lista_tareas,
+#                'responsable':emailUsuario
+#            })
         else:
             return render(request,'gestion_tareas/ingresar.html',{
                 'mensaje_error':'Los datos ingresados son incorrectos',
@@ -40,12 +67,32 @@ def ingresar(request):
 #        print(passwordUsuario)
     #return render(request,'gestion_tareas/ingresar.html')
 
-def dashboard(request):
-    
+def dashboard(request,responsable):
     #filtrar pichangas de nuestro equipo
-
+    lista_tareas = tarea.objects.filter(responsable=responsable)
+    lista_tareas = lista_tareas.exclude(estado='ELIMINADO')
+    if request.method == 'POST':        
+        fecha_inicio = request.POST.get("fecha_inicio")
+        print(fecha_inicio)
+        #fecha_inicio = parse(fecha_inicio)
+        print(fecha_inicio)
+        fecha_fin = request.POST.get("fecha_fin")
+        print(fecha_inicio)
+        #fecha_fin = parse(fecha_fin)
+        print(fecha_inicio)
+        if fecha_inicio != "" and fecha_fin != "":
+            lista_tareas = tarea.objects.filter(fecha_cracion__range=[fecha_inicio,fecha_fin],responsable=responsable)
+            lista_tareas = lista_tareas.exclude(estado='ELIMINADO')
+        return render(request,'gestion_tareas/dashboard.html',{
+            'lista_tareas':lista_tareas,
+            'responsable':responsable 
+        })
     #filtrar finalizado
-    return render(request,'gestion_tareas/dashboard.html')
+#    return render(request,'gestion_tareas/dashboard.html')
+    return render(request,'gestion_tareas/dashboard.html',{
+        'lista_tareas':lista_tareas,
+        'responsable':responsable 
+    })
 
 def detalle_tarea(request,ind):
     tarea_editar = tarea.objects.get(id=ind)
@@ -58,12 +105,61 @@ def editar_tarea(request,ind):
     if request.method == 'POST':
         descripcion = request.POST.get('descripcion')
         fecha_creacion = request.POST.get('fecha_creacion')
+        fecha_creacion = parse(fecha_creacion)
         fecha_entrega = request.POST.get('fecha_entrega')
+        fecha_entrega = parse(fecha_entrega)
         tarea_editar.descripcion = descripcion
         tarea_editar.fecha_cracion = fecha_creacion
         tarea_editar.fecha_entrega = fecha_entrega
         tarea_editar.save()
-        return HttpResponseRedirect(reverse('gestion_tareas:dashboard'))
-    return render(request,'gestion_tareas/editar_tarea.html',{
-        'tarea':tarea_editar 
+        lista_tareas = tarea.objects.filter(responsable=tarea_editar.responsable)
+        lista_tareas = lista_tareas.exclude(estado='ELIMINADO')
+        #return HttpResponseRedirect(reverse('gestion_tareas:dashboard'))
+        return render(request,'gestion_tareas/dashboard.html',{
+            'lista_tareas':lista_tareas,
+            'responsable':tarea_editar.responsable  
     })
+    return render(request,'gestion_tareas/editar_tarea.html',{
+        'tarea':tarea_editar,
+#        'lista_tareas':tarea.objects.all() 
+    })
+
+def crear_tarea(request,responsable):
+    if request.method == 'POST':
+        descripcion = request.POST.get('descripcion')
+        fecha_entrega = request.POST.get('fecha_entrega')
+        fecha_entrega = parse(fecha_entrega)
+        #responsable = request.POST.get('responsable')
+        tarea(descripcion=descripcion,fecha_entrega=fecha_entrega,responsable=responsable).save()
+        lista_tareas = tarea.objects.filter(responsable=responsable)
+        lista_tareas = lista_tareas.exclude(estado='ELIMINADO')
+        #return HttpResponseRedirect(reverse('gestion_tareas:dashboard'))
+        return render(request,'gestion_tareas/dashboard.html',{
+            'lista_tareas':lista_tareas,
+            'responsable':responsable 
+    })
+    return render(request,'gestion_tareas/crear_tarea.html',{
+        'responsable':responsable 
+    })      
+
+def eliminar_tarea(request,ind):
+    tarea_eliminar = tarea.objects.get(id=ind)
+    tarea_eliminar.estado = "ELIMINADO"
+    tarea_eliminar.save()
+    lista_tareas = tarea.objects.filter(responsable=tarea_eliminar.responsable)
+    lista_tareas = lista_tareas.exclude(estado='ELIMINADO')
+    return render(request,'gestion_tareas/dashboard.html',{
+        'lista_tareas':lista_tareas,
+        'responsable':tarea_eliminar.responsable    
+    })      
+
+def finalizar_tarea(request,ind):
+    tarea_eliminar = tarea.objects.get(id=ind)
+    tarea_eliminar.estado = "FINALIZADO"
+    tarea_eliminar.save()
+    lista_tareas = tarea.objects.filter(responsable=tarea_eliminar.responsable)
+    lista_tareas = lista_tareas.exclude(estado='ELIMINADO')
+    return render(request,'gestion_tareas/dashboard.html',{
+        'lista_tareas':lista_tareas,
+        'responsable':tarea_eliminar.responsable    
+    })          
